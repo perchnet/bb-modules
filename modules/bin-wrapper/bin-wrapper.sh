0b1dde9d77989c8a5c33b8db664d418c028a849f
bri <284789+b-@users.noreply.github.com>
2025-02-18 19:23:36 -0500

    refactor(bin-wrapper): streamline bin-wrapper script and remove unused code

--- modules/bin-wrapper/bin-wrapper.sh
+++ modules/bin-wrapper/bin-wrapper.sh
@@ -6,7 +6,6 @@ set -euxo pipefail
 # It is useful for adding flags to a binary that is called by a link, or for adding flags to a binary that is called by a script.
 
 BIN_WRAPPER_URL="https://raw.githubusercontent.com/perchnet/bin-wrapper/refs/heads/main/src/wrap-bin.sh"
-#BIN_WRAPPER_URL="file:///${HOME}/dev/bin-wrapper/src/wrap-bin.sh"
 ### USAGE ###
 # modules:
 #   - type: bin-wrapper
@@ -14,29 +13,7 @@ BIN_WRAPPER_URL="https://raw.githubusercontent.com/perchnet/bin-wrapper/refs/hea
 #     flags:
 #       - --private-window
 #       - --new-tab https://example.com
-#
-# get_json_array() {
-#   local -n arr="${1}"
-#   local jq_query="${2}"
-#   local module_config="${3}"
-# 
-#   if [[ -z "${jq_query}" || -z "${module_config}" ]]; then
-#     echo "Usage: get_json_array VARIABLE_TO_STORE_RESULTS JQ_QUERY MODULE_CONFIG" >&2
-#     return 1
-#   fi
-#
-#   readarray -t arr < <(echo "${module_config}" | jq -c -r "${jq_query}")
-# }
-#
-#
-# yaml='---
-# type: bin-wrapper
-# bin: /home/bri/dev/desk/modules/bin-wrapper/testdata/executable
-# flags:
-#   - --private-window
-#   - --new-tab https://example.com
-# '
-# module_config="$(echo "$yaml" | yq eval -o=j -I=0)"
+
 JSON_ARRAY="${1:?Usage: bin-wrapper.sh <module_config>}"
 get_json_array BIN 'try .["bin"]' "${module_config:-"${JSON_ARRAY}"}"
 get_json_array FLAGS 'try .["flags"][]' "${module_config:-"${JSON_ARRAY}"}"
@@ -44,10 +21,8 @@ get_json_array FLAGS 'try .["flags"][]' "${module_config:-"${JSON_ARRAY}"}"
 echo "BIN: ${BIN}"
 echo "FLAGS: ${FLAGS[*]}"
 
-#FLAGS=("--private-window" "--new-tab https://example.com")
-TMP_BIN_WRAPPER="$(mktemp)"
-curl -sSL "${BIN_WRAPPER_URL}" -o "${TMP_BIN_WRAPPER}"
-chmod +x "${TMP_BIN_WRAPPER}"
-<"${TMP_BIN_WRAPPER}" tee /tmp/ffflog
-"${TMP_BIN_WRAPPER}" "${BIN}" "${FLAGS[@]}"
-< "${BIN}" tee -a /tmp/ffflog
+# download the bin-wrapper script
+TMP_BIN_WRAPPER="$(mktemp)" # make a temp file
+curl -sSL "${BIN_WRAPPER_URL}" -o "${TMP_BIN_WRAPPER}" # write it to the temp file
+chmod +x "${TMP_BIN_WRAPPER}" # make it executable
+"${TMP_BIN_WRAPPER}" "${BIN}" "${FLAGS[@]}" # run the bin-wrapper script with the bin and flags
